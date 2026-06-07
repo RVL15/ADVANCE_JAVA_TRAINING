20d5f8690058bf0486f75fe0c9a97e4ade7b8961
RISHABH LOKHANDE <rishabhvlokhande1@gmail.com>
2026-06-07 11:03:47 +0530

    Updated Java programs

---
+++ HW/PATTERNS/Armstrong.java
@@ -0,0 +1,21 @@
+public class Armstrong {
+    public static void main(String[] args) {
+
+        int n = 153;
+        int temp = n;
+        int sum = 0;
+
+        while (n > 0) {
+
+            int rem = n % 10;
+            sum = sum +rem * rem * rem;
+            n = n / 10;
+        }
+
+        if (temp == sum) {
+            System.out.println(temp + " is an Armstrong Number");
+        } else {
+            System.out.println(temp + " is not an Armstrong Number");
+        }
+    }
+}
